40ef72efb99e867f2ea8fd9b4c65257d4dcfd72c
Nikolai <nikolai.varichev@gmail.com>
2020-03-23 19:33:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict';
+// let arr = ['plum.png', 'orangen.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+let answer = prompt("Are you 18?", "Yes / No");
+console.log(answer);
